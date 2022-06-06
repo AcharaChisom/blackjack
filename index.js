10dfa7cda6961cc20d5bc580d6c752acb0997b6c
Achara Chisom <acharachisom28@gmail.com>
2022-06-06 02:14:05 +0100

    add js code to make the startGame button functional and display the sum and cards

---
+++ index.js
@@ -0,0 +1,25 @@
+let firstCard = 10
+let secondCard = 12
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+
+    cardsEl.textContent += firstCard + ", " + secondCard
+    messageEl.textContent = message
+    sumEl.textContent += sum
+}
